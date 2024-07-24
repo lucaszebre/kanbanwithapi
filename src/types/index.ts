8548c64a0f139cf74b8db78020193649e6eb620f
Lucas Zebre <yakirisk@gmail.com>
2024-07-24 11:06:27 +0200

    handle problem in register / login

--- src/types/index.ts
+++ src/types/index.ts
@@ -145,10 +145,8 @@ export interface Board {
 
         export const SchemaLogin = z.object({
             email: z.string().min(1,{ message: 'need a username' }),
-            password: z.string().min(8, { message: 'at least 8 characters long' })
-            .regex(/[A-Za-z]/, { message: ' must contain at least one letter' })
-            .regex(/[0-9]/, { message: ' must contain at least one digit' })
-            .regex(/[!@#$%^&*(),.?":{}|<>]/, { message: '  at least one special character' }),
+            password: z.string().min(1, { message: 'at least 1 characters long' })
+            ,
           });
           
           export interface FormDataRegister {
@@ -169,10 +167,8 @@ export interface Board {
   export const SchemaRegister = z.object({
     email: z.string().email().min(1,{ message: 'need a email' }),
     name: z.string().min(1,{ message: 'need a first name' }),
-    password: z.string().min(8, { message: 'at least 8 characters long' })
-    .regex(/[A-Za-z]/, { message: ' must contain at least one letter' })
-    .regex(/[0-9]/, { message: ' must contain at least one digit' })
-    .regex(/[!@#$%^&*(),.?":{}|<>]/, { message: '  at least one special character' }),
+    password: z.string().min(1, { message: 'at least 1 characters long' })
+    ,
   });
   
   export const AddTask = z.object({
