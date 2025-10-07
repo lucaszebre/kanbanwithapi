38a116e008b1ac3e3c5757b8a2aa3c89dc6360eb
lucaszebre <lucaszebre1@gmail.com>
2025-10-07 21:17:01 +0200

    feat: add AddTaskSchema for task validation using Zod

---
+++ src/types/AddTaskSchema.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const AddTaskSchema = z.object({
+  title: z.string().min(1, { message: "Title is required." }),
+  description: z.string().min(1, { message: "description is required" }),
+  columnId: z.string().min(1, { message: "Column is required." }),
+  subtasks: z.array(
+    z.object({
+      id: z.string(),
+      title: z.string().min(1, { message: "Subtask cannot be empty." }),
+      isCompleted: z.boolean(),
+      index: z.number(),
+    })
+  ),
+});
