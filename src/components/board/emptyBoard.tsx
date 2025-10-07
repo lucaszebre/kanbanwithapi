586e59a33a7e4efd8746eac858dbe41f9126e832
lucaszebre <lucaszebre1@gmail.com>
2025-10-07 21:16:27 +0200

    refactor: simplify EmptyBoard component by removing local state and handling board editing directly

--- src/components/board/emptyBoard.tsx
+++ src/components/board/emptyBoard.tsx
@@ -1,15 +1,12 @@
 import { useTaskManagerStore } from "@/state/taskManager";
-import { useMemo, useState } from "react";
+import { useMemo } from "react";
 import { useTranslation } from "react-i18next";
 import { useParams } from "react-router";
 import { Button } from "../ui/button";
-import { AddBoard } from "./addBoard";
 import { EditBoard } from "./editBoard";
 
 export const EmptyBoard = () => {
   const { t } = useTranslation("board");
-  const [addBoard, setAddBoard] = useState(false);
-  const [editBoard, setEditBoard] = useState(false);
   const taskManager = useTaskManagerStore((s) => s.taskManager);
   const { boardId } = useParams();
 
@@ -28,27 +25,14 @@ export const EmptyBoard = () => {
   const buttonClasses =
     "bg-[#635FC7] text-white rounded-md px-5 py-2.5 text-[14px] font-medium cursor-pointer transition-colors  h-12 hover:brightness-110";
 
-  const handleClick = () => {
-    if (currentBoard) setAddBoard(true);
-    else setEditBoard(true); // editing current board to add columns
-  };
-
   return (
     <>
-      {currentBoard ? (
-        <AddBoard addBoard={addBoard} setAddBoard={setAddBoard} />
-      ) : (
-        <EditBoard
-          board={currentBoard}
-          editBoard={editBoard}
-          setEditBoard={setEditBoard}
-        />
-      )}
       <div className={containerClasses}>
         <h1 className={titleClasses}>{t("empty.title")}</h1>
-        <Button onClick={handleClick} className={buttonClasses}>
-          {t("empty.button")}
-        </Button>
+
+        <EditBoard board={currentBoard}>
+          <Button className={buttonClasses}>{t("empty.button")}</Button>
+        </EditBoard>
       </div>
     </>
   );
