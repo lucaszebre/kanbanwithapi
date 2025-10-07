0b11d888a52d17508984d9e00bc7a90cb2f96af6
lucaszebre <lucaszebre1@gmail.com>
2025-10-07 21:16:37 +0200

    refactor: simplify NoBoards component by removing local state and props from AddBoard

--- src/components/board/noBoards.tsx
+++ src/components/board/noBoards.tsx
@@ -1,26 +1,24 @@
 import { Button } from "@/components/ui/button";
-import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { AddBoard } from "./addBoard";
 
 export const NoBoards = () => {
   const { t } = useTranslation("board");
-  const [addBoard, setAddBoard] = useState(false);
 
   return (
     <>
-      <AddBoard addBoard={addBoard} setAddBoard={setAddBoard} />
       <div className="m-auto flex h-full w-full flex-col items-center justify-center gap-y-4">
         <h2 className="text-center text-lg font-bold ">
           {t("noBoardsMessage")}
         </h2>
-        <Button
-          variant={"secondary"}
-          onClick={() => setAddBoard(true)}
-          className="cursor-pointer rounded-3xl p-4"
-        >
-          {t("addBoardButton")}
-        </Button>
+        <AddBoard>
+          <Button
+            variant={"secondary"}
+            className="cursor-pointer rounded-3xl p-4"
+          >
+            {t("addBoardButton")}
+          </Button>
+        </AddBoard>
       </div>
     </>
   );
